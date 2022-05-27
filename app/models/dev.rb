f88e61f866d2d4d393229cc2ebf6e29a65ad7ee0
Brian <b.perez3237@gmail.com>
2022-05-27 15:52:37 -0400

    commit tree 2703e22a0b0bf7d73e96bf967c8d058f7861c2e8

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one(item_name)
+        self.freebies.select {|freeb| freeb.item_name==item_name} == [] ? false : true
+    end
+
+    def give_away(dev, freebie)
+        if self.freebies.include? freebie
+            freebie.dev_id = dev.id
+            self.freebies = self.freebies.select {|freeb| freeb.id != freebie.id}
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,9 @@ puts "Creating freebies..."
 # ***************************************************************
 # Create freebies Here
 
+Freebie.create(item_name: "freebie 1", value: 1, dev_id: Dev.find_by(name: "Rick").id, company_id: Company.find_by(name: "Google").id)
+Freebie.create(item_name: "freebie 2", value: 4, dev_id: Dev.find_by(name: "Rick").id, company_id: Company.find_by(name: "Facebook").id)
+Freebie.create(item_name: "freebie 3", value: 8, dev_id: Dev.find_by(name: "Morty").id, company_id: Company.find_by(name: "Enron").id)
+Freebie.create(item_name: "freebie 4", value: 1, dev_id: Dev.find_by(name: "Gazorpazop").id, company_id: Company.find_by(name: "Dunder Mifflin").id)
+
 puts "Seeding done!"
